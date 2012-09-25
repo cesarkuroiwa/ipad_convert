641e776731fa936051c4ae2d485fcc4cf07de5e3
cesar <cesar@cesar.in.registro.br>
2012-09-25 10:33:49 -0300

    indentation use subtitles in utf8

--- ipad_convert.sh
+++ ipad_convert.sh
@@ -5,13 +5,14 @@ output=$2
 
 if [ -z "$input" -o -z "$output" ]
 then
-	echo "Usage: $0 <input_file> <output_file> [<subtitle_file>]"
-	exit 1
+		echo "Usage: $0 <input_file> <output_file> [<subtitle_file>]"
+		exit 1
 fi
 
+subtitle=""
 if [ -n "$3" ]
 then
-	subtitle="-sub $3"
+		subtitle="-utf8 -sub $3"
 fi
 
 mencoder -ovc x264 -x264encopts global_header:bframes=0 -oac faac -faacopts br=160:mpeg=4:object=2:raw -of lavf $input -o $output $subtitle
